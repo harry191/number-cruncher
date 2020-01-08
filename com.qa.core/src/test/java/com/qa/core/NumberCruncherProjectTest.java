d09835af8739f54844f7807268eb338f58270d67
H <thisisafakeemail@lol.com>
2020-01-08 10:01:10 +0000

    inital push

---
+++ com.qa.core/src/test/java/com/qa/core/NumberCruncherProjectTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.qa.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Admin
+ *
+ */
+class NumberCruncherProjectTest {
+
+	@Test
+	void NumberCrunchTest() {
+		int input[] = {3};
+	}
+
+}
