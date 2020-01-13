9256a226132d4cfdf5b9049d4adb818be46f91af
H <thisisafakeemail@lol.com>
2020-01-13 09:26:25 +0000

    commit

--- com.qa.core/src/main/java/com/qa/core/NumberCruncherProject.java
+++ com.qa.core/src/main/java/com/qa/core/NumberCruncherProject.java
@@ -3,13 +3,31 @@ package com.qa.core;
 public class NumberCruncherProject {
 
 	
-	public int findTheBigNumber(int[] input) {
+	public int findTheBigNumber(int[] input) throws MyException {
+		
+		
+		//if(input.length < 0) {
+			//throw new MyExeception("Array length needs to be greater than 0");
+		//}
+		
+		
 		for (int i = 0; i < input.length; i++) {
 			
-			if(input)
+			if(input.length<1) {
+				throw new MyException("Array must contain something");
+			}
 			
-		}
-		return 0;
+			if(input.length == 1) {
+				return input[0];
+			}else if(input[0]<input[1]){
+				return input[1];
+			}else {
+				return input[0];
+			}
+			
+		}return 0;
+		
+		
 		
 		
 		}
--- com.qa.core/src/test/java/com/qa/core/NumberCruncherProjectTest.java
+++ com.qa.core/src/test/java/com/qa/core/NumberCruncherProjectTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class NumberCruncherProjectTest {
 
 	@Test
-	void find_highest_number_in_array_of_one() {
+	void find_highest_number_in_array_of_one() throws MyException {
 		int input[] = {3};
 		int expResult = 3;
 		NumberCruncherProject nc = new NumberCruncherProject();
@@ -17,7 +17,7 @@ class NumberCruncherProjectTest {
 	}
 	
 	@Test
-	void find_highest_number_in_array_of_two() {
+	void find_highest_number_in_array_of_two() throws MyException {
 		int input[] = {4 , 2};
 		int expResult = 4;
 		NumberCruncherProject nc = new NumberCruncherProject();
@@ -27,13 +27,23 @@ class NumberCruncherProjectTest {
 	}
 	
 	@Test
-	void find_highest_number_in_array_of_two_mixed() {
+	void find_highest_number_in_array_of_two_mixed() throws MyException {
 		int input[] = {7 , 13};
 		int expResult = 13;
 		NumberCruncherProject nc = new NumberCruncherProject();
 		int result = nc.findTheBigNumber(input);
 		
 		assertEquals(expResult, result);
+	}
+	
+	@Test
+	void find_highest_number_in_array_of_none() {
+		int input[] = {};
+		NumberCruncherProject nc = new NumberCruncherProject();
+		
+		assertThrows(MyException.class,
+				() -> nc.findTheBigNumber(input) );
+	
 	}
 
 }
